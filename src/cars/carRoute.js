42413b1b008d824bbfd31cc43529553176e807bd
openwell <timileyinojo@yahoo.com>
2019-07-16 15:36:59 +0100

    added two routes to update and delete  a car

--- src/cars/carRoute.js
+++ src/cars/carRoute.js
@@ -27,4 +27,28 @@ router.post("/", (req, res) => {
   }
 });
 
+router.put("/:id", (req, res) => {
+  try {
+    db.update(req.params.id, req.body).then(data => {
+      return res.status(200).json({
+        data: data
+      });
+    });
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  try {
+    db.remove(req.params.id).then(data => {
+      return res.status(200).json({
+        data: data
+      });
+    });
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
 module.exports = router;
