6d4b82457bbaeaca236e3c37ecc452a0633f208a
openwell <timileyinojo@yahoo.com>
2019-07-16 13:59:54 +0100

    app setup and setting up of database

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+server.use(express.json());
+
+server.use(logger, helmet());
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+server.all("*", (req, res) => {
+  res.status(404).json("Sorry No Such Location");
+});
+//custom middleware
+
+function logger(req, res, next) {
+  console.log(req.method, req.url, Date.now());
+  next();
+}
+
+module.exports = server;
